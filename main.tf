09206eba9e71b77d7b0b5273f787ea5ba7f661f5
kishanpatelprime <149482996+kishanpatelprime@users.noreply.github.com>
2023-11-01 12:40:07 +1030

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,54 +1,54 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIA4I"
-  secret_key = "K8jghezj//uAjEB9"
+  access_key = "AKIAD7BFY"
+  secret_key = "KRU1F"
 }
 
 resource "aws_instance" "newinstance" {
-    ami = ami-0dbc3d7bc646e8516
-    instance_type = "t2.micro"
+    ami = "0dbc3d7bc646e8516"  # we can use variable ami optiom as well
+    instance_type = "t2.micro" 
 }
 
 resource "aws_vpc" "my-vpc" {
-    cidr_block = "192.0.0.0/16"
+    cidr_block = "192.168.0.0/16"
 
     tags ={
         name = "my-vpc"
     }
   
 }
-resource "aws_subnet" "private1" {
-    vpc_id = aws_vpc.my-vpc
-    cidr_block = "192.0.0.1/24"
-    availability_zone = "us-east-1a"
+resource "aws_subnet" "private1" {   # resource name must be unique
+    vpc_id = "aws_vpc.my-vpc"
+    cidr_block = "192.0.1.0/24"
+    availability_zone = "us-west-2a"
     tags = {
       name= "private-subnet1"
     }
   
 }
 resource "aws_subnet" "private2" {
-    vpc_id = aws_vpc.my-vpc
-    cidr_block = "192.0.0.3/24"
+   vpc_id = "aws_vpc.my-vpc"
+    cidr_block = "192.168.3.0/24"
     
-    availability_zone = "us-east-1b"
+    availability_zone = "us-west-2b"
     tags = {
       name= "private-subnet2"
     }
   
 }
 resource "aws_subnet" "public1" {
-    vpc_id = aws_vpc.my-vpc
-    cidr_block = "192.0.0.3/24"
-    availability_zone = "us-east-1a"
+   vpc_id = "aws_vpc.my-vpc"
+    cidr_block = "192.168.2.0/24"
+    availability_zone = "us-west-2a"
     tags = {
       name= "public-subnet1"
     }
   
 }
 resource "aws_subnet" "public2" {
-    vpc_id = aws_vpc.my-vpc
-    cidr_block = "192.0.0.4/24"
-    availability_zone = "us-east-1b"
+   vpc_id = "aws_vpc.my-vpc"
+    cidr_block = "192.168.4.0/24"
+    availability_zone = "us-west-2b"
     tags = {
       name= "public-subnet2"
     }
